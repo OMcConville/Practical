2ccccdee1d51f72ae8f5a4f3cd75b207e9256fe9
OMcConville <mcconville-o6@ulster.ac.uk>
2022-02-17 15:55:21 +0000

    Update HomeController.cs

--- SMS.Web/Controllers/HomeController.cs
+++ SMS.Web/Controllers/HomeController.cs
@@ -2,20 +2,22 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using SMS.Web.Models;
 
-namespace SMS.Web.Controllers;
+namespace SMS.Web.Controllers
+{
 
-public class HomeController : Controller
-{
-   public IActionResult Index()
+   public class HomeController : Controller
    {
-       return Content("Web Development");
+      public IActionResult Index()
+      {
+         return Content("Web Development");
+      }
+      public IActionResult About()
+      {
+         // construct the view model
+         var about = new AboutViewModel {
+         Formed = new DateTime(2020, 01, 01)      
+         };
+         return View(about);
+      }
    }
-   public IActionResult About()
-{
-   // construct the view model
-   var about = new AboutViewModel {
-     Formed = new DateTime(2020, 01, 01)      
-   };
-   return View(about);
-}
 }
